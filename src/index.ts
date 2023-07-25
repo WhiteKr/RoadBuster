087a675b8b3a8b369f8813d1052667e1da8c7487
whitekr <hanjisu2215g@gmail.com>
2023-07-25 17:15:43 +0900

    update: separate events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { CacheType, Client, Collection, Events, GatewayIntentBits, Interaction } from 'discord.js';
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import { token } from './config.json';
 
 const client: Client<boolean> = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -25,32 +25,5 @@ for (const folder of commandFolders) {
         }
     }
 }
-
-client.once(Events.ClientReady, (c: Client<true>) => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.InteractionCreate, async (interaction: Interaction<CacheType>) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    }
-});
 
 client.login(token);
