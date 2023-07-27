6ccecd22956caadfaa5d804b9073ba7c7a7a332b
whitekr <hanjisu2215g@gmail.com>
2023-07-27 16:09:27 +0900

    fix: catch null exception and color embed

--- src/commands/utilities/info.ts
+++ src/commands/utilities/info.ts
@@ -18,20 +18,29 @@ module.exports = {
         if (!interaction.isChatInputCommand()) return;
 
         const user: User | null = interaction.options.getUser('user');
+        if (user === null) {
+            return interaction.reply({
+                embeds: [{
+                    color: 0xff0000,
+                    title: '오류',
+                    description: '유저를 찾을 수 없습니다. 다시 시도하세요.',
+                }],
+            });
+        }
 
         const infoEmbed: EmbedBuilder = new EmbedBuilder();
         infoEmbed
+            .setColor(0x00ff00)
             .setAuthor({
                 name: interaction.user.username,
                 iconURL: interaction.user.displayAvatarURL(),
             })
-            .setTitle(`${user?.username} 정보`)
+            .setTitle(`${user.username} 정보`)
             .setDescription(`${user}`)
             .addFields(
                 { name: '순위', value: '4', inline: true },
                 { name: '티어', value: 'S+', inline: true },
-            )
-            .setFooter({ text: 'RoadBuster by @whitekr' });
+            );
 
         await interaction.reply({ embeds: [infoEmbed] });
     },
